0cf2643829efd8ff08c81f4921e3dfaff65a6417
Jay <jayliu@zymber.tech>
2023-04-19 16:58:58 +0800

    + 6 JSX (下)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* 
-【React.js入門 - 05】 JSX (上) - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10215841
+【React.js入門 - 06】 JSX (下) - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10216468
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -8,31 +8,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const showOne = true;
-const showOne = false;
-const styleArgument = { fontSize: '100px', color: 'red' };
-const multiButton=()=>{
-  var output=[];
-  for(let i=0;i<4;++i)
-      output.push(<button>我是第{i}個按鍵</button>)
-  return output;
+const getValue=(event)=>{
+  console.log(event.target.value)
+}
+const getValue2=(value)=>{
+  console.log(value)
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <h1> { (showOne==true) ? 1 : 0 } </h1>
-  
-  // <h1 style = { styleArgument } > Hello, world! </h1>
-
-  // <h1 style = {{ fontSize: '100px', color: 'red' }} > Hello, world! </h1>
-
-  //這是錯誤的寫法
-  // <h1 class = "title" > Hello, world! </h1>
-  //這是正確的寫法
-  // <h1 className = "title" > Hello, world! </h1>
-
   <div>
-    { multiButton() }
+    <button value={true} > 是 </button>
+    <button value > 是 </button>
+
+    <button value="true" onclick="myFunction(參數)">是</button>
+    <button value={true}  onClick={ null }>是</button>
+    <input type="text" onChange={ null }/>
+
+    <button value="true" onclick="myFunction(參數)">是</button>
+    <button value={true} onClick={getValue}>按下以取得數值 </button>
+    <button value={true} onClick={(event)=>{getValue2(event.target.value)}}>
+            按下以取得數值 
+    </button>
   </div>
 );
 
