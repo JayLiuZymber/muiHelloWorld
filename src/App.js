b5d9efad7a73cd10803b146b2ffc2b74b623573e
Jay <jayliu@zymber.tech>
2023-04-20 10:13:41 +0800

    + 10 夾在中間的props: children

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App(props){ // 在函式參數中加入props
   return(
-    <button onClick={props.handleClick}>{props.name}</button>
+    <button> {props.children} </button> 
   );
 }
 export default App; //輸出App函式
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 /* 
-【React.js入門 - 09】 用props綁定函式 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10218096
+【React.js入門 - 10】 夾在中間的props: children - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10218605
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Layout from './Layout';
 import reportWebVitals from './reportWebVitals';
-
-const printMessage=()=>{
-  document.getElementById('show-area').innerHTML="我被按到了";
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //在App標籤中加入name屬性
   <div>
-    <App name="我的名字" handleClick={printMessage}/> {/* 綁定函式 */}
-    <div id="show-area"></div>    {/* 這裡是等等用來改變的地方 */}
+    <App> 在index.js中設定文字 </App>
+    <Layout>
+      <App>在index.js中設定文字</App>
+    </Layout>
   </div>
 );
 
