840ca1cff4496924822233f25ef676994bfcc017
Jay <jayliu@zymber.tech>
2023-04-20 15:55:53 +0800

    + 15 使用Http request - Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,107 @@
 import React, { Component } from 'react';
 
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
     this.state={
-      percent:"30%"
+      repoName: null
     }
-    this.name="舊的名字";
-    this.changePercent=this.changePercent.bind(this);
+    this.handleClick=this.handleClick.bind(this);
   }
+  /* // 加上headers(1/2) - 設定Content-Type
+  fetch( request的url, {
+        method: "GET",
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+      // 接到request data後要做的事情
+    })
+    .catch(e => {
+      // 發生錯誤時要做的事情
+    })
+   */
+  /* // 加上headers(2/2) - 我要傳token!!!!!!!
+  const token = "Bearer "+ 我存好的token ;
+  fetch( request的url, {
+      method: "GET",
+      headers: new Headers({
+          'Content-Type': 'application/json',
+          'Authorization': token, // 把token放在這
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+          // 接到request data後要做的事情
+    })
+    .catch(e => {
+        // 發生錯誤時要做的事情
+    })
+   */
+  /* // 加上body(1/2) - 使用JSON type傳送資料
+  const data= { A:"資料A", B:"資料B" }
+  fetch( request的url, {
+      method: "GET",
+      body: JSON.stringify(data),   // 把json資料字串化
+      headers: new Headers({
+          'Content-Type': 'application/json'
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      // 接到request data後要做的事情
+    })
+    .catch(e => {
+      // 發生錯誤時要做的事情
+    })
+   */
+  /* // 加上body(2/2) - 使用x-www-form-urlencoded type傳送資料
+  const data= { A:"資料A", B:"資料B" };
+  const formData = Object.keys(data).map(
+      function (keyName) {
+          return encodeURIComponent(keyName) + '=' + encodeURIComponent(data[keyName])
+      }
+  ).join('&');
+  fetch( request的url, {
+      method: "GET",
+      body: formData,   // 使用處理後的資料
+      headers: new Headers({
+          "Content-type": "application/x-www-form-urlencoded"
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      // 接到request data後要做的事情
+    })
+    .catch(e => {
+      // 發生錯誤時要做的事情
+    })
+   */
 
-  changePercent(){
-    this.setState({percent:(this.state.percent==="70%")?"30%":"70%"})
-    console.log("hey")
+  handleClick(){
+    fetch( 'https://api.github.com/users/jserv/repos',{method:"GET"})
+    .then(res => res.json())
+    .then(data => {
+          /*接到request data後要做的事情*/
+          this.setState({repoName: data[0]['name']});
+    })
+    .catch(e => {
+        /*發生錯誤時要做的事情*/
+        console.log(e);
+    })
   }
-
-  render(){
-    return(
-      <div>
-        <div className="progress-back" style={{backgroundColor:"rgba(0,0,0,0.2)",width:"200px",height:"7px",borderRadius:"10px"}}>
-          <div className="progress-bar" style={{backgroundColor:"#fe5196",width:this.state.percent,height:"100%",borderRadius:"10px"}}></div>
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="data-display">
+          {(this.state.repoName===null)?"目前還有沒有資料":this.state.repoName}
         </div>
-        <button onClick={this.changePercent}> {this.props.children} </button>
-        {this.props.name}
-      </div>
-    );
+        <button onClick={this.handleClick}>取得jserv以英文字母排序的第一個repo</button>
+    	</div>
+    )
   }
-}
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 /* 
-【React.js入門 - 14】 Debug利器 : React-Developer-Tools - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10220526
+【React.js入門 - 15】 使用Http request - Fetch Api - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10221020
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-/* 
-React 開發者工具 • React
-https://beta.zh-hant.reactjs.org/learn/react-developer-tools
- */
+
+// 瀏覽器->按下按鈕->F12->網路->回應
+// 0	Object { id: 497792878, node_id: "R_kgDOHau3bg", name: "123elf", … }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App name="我是進度條">在index.js中轉換百分比</App>
+    <App />
   </div>
 );
 
