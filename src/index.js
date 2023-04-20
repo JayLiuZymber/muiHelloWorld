02d9fcfe7d10d1e35ee64e3878a4a535159307bc
Jay <jayliu@zymber.tech>
2023-04-20 14:23:41 +0800

    + 13 useState - 在function component用state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 /* 
-【React.js入門 - 12】 state 與 詳解setState語法 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10219561
+【React.js入門 - 13】 useState - 在function component用state - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10220063
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import App2 from './App2';
-import App3 from './App3';
+import Progress from './Progress';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <App /><br />
-    <App2 />
-    <App3 />
+    <App />
+    <Progress />
   </div>
 );
 
