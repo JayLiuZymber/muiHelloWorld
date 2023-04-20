1d4dedb08ad6cd6e7d2d253acf2f6163b0053d8c
Jay <jayliu@zymber.tech>
2023-04-20 09:37:04 +0800

    + 9 用props綁定函式

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 function App(props){ // 在函式參數中加入props
   return(
-    <button> {props.name} </button>
-    /*修改button標籤中間的內容，注意這裡要使用JSX的{}才能使用js資料*/
+    <button onClick={props.handleClick}>{props.name}</button>
   );
 }
 export default App; //輸出App函式
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* 
-【React.js入門 - 08】 用props綁定資料 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10217533
+【React.js入門 - 09】 用props綁定函式 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10218096
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -8,31 +8,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function App2(props){
-  return(
-    <button> {props.number +' '+ props.getData} </button>
-  );
-}
-function App3(props){
-  return(
-    <button> {`${props.number} ${props.getData}`} </button>
-  );
-}
-// props不能被元件自己更改(read-only)
-function App4(props){
-  return(
-    <button> {this.props.number=88} </button>
-  );
+const printMessage=()=>{
+  document.getElementById('show-area').innerHTML="我被按到了";
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //在App標籤中加入name屬性
   <div>
-    <App name="我的名字"/>
-    <App2 number="87" getData="true"/>
-    <App3 number={87} getData={true}/>
-    {/* <App4 number={87} getData={true}/> */}
+    <App name="我的名字" handleClick={printMessage}/> {/* 綁定函式 */}
+    <div id="show-area"></div>    {/* 這裡是等等用來改變的地方 */}
   </div>
 );
 
