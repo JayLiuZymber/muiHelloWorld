9dc155336270450e91278007e757e40ed91f7566
Jay <jayliu@zymber.tech>
2023-04-21 16:08:29 +0800

    + 25 監控瀏覽器長寬 - 以React hook實現

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* 
-【React.js入門 - 24】 Custom hook - 給我另一個超推React hook的理由 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10224881
+【React.js入門 - 25】 監控瀏覽器長寬 - 以React hook實現 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10225184
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
