8e0fd42d6622a0a47a24009dd618aa2788d700c4
Jay <jayliu@zymber.tech>
2023-04-21 17:49:00 +0800

    + 27 我要更多更多的分頁 - react-router-dom (上)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import LoginForm from './LoginForm';
+/*
+終究都要學 React 何不現在學呢？ - React Router - 基礎與介紹 - (24) - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10303610?sc=rss.iron
+
+安裝react-router-dom v5
+$ npm i react-router-dom@5.3.4
+ */
+import {HashRouter,Route,Switch} from "react-router-dom";
+import FirstPage from "./FirstPage";
+import SecondPage from "./SecondPage";
+import ThirthPage from './ThirthPage';
 
 const App=()=>{
     return(
-        <LoginForm />
+        <HashRouter>
+            <Switch>
+                {/* <Route path="路徑" component={ 用來當頁面的元件名稱(不是標籤) } /> */}
+                {/* <Route path="/" component={FirstPage}/> */}
+                {/* <Route path="/second" component={SecondPage}/> */}
+
+                {/* http://localhost:3000/#/ */}
+                <Route exact path="/" component={FirstPage}/>
+                {/* http://localhost:3000/#/second */}
+                <Route exact path="/second" component={SecondPage}/>
+                {/* http://localhost:3000/#/3th123 */}
+                {/* http://localhost:3000/#/3th */}
+                <Route exact path="/3th:id" component={SecondPage}/>
+                <Route exact path="/3th:id?" component={ThirthPage}/>
+            </Switch>
+        </HashRouter>
     );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 /* 
-【React.js入門 - 26】 input使用、input與state的互動 (控制組件) 、其他輸入元素 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10225692
+【React.js入門 - 27】 我要更多更多的分頁 - react-router-dom (上) - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10226056
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import App6 from './App6';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <App />
+    {/* <App6 /> */}
   </div>
 );
 
