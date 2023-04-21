392281c4f6ce5dd4fb005b1de0a7107824e13ee2
Jay <jayliu@zymber.tech>
2023-04-21 15:29:16 +0800

    + 22 元件練習(上) - 在class利用遞迴+state實作動畫

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Parent from './Parent';
+import React,{useState} from 'react';
+import ProgressDIY from './ProgressDIY'
 
 const App=()=>{
+    const [value,setValue]=useState(10);
     return(
-        <div>
-            <Parent />
-        </div>
+        <ProgressDIY value={value} onClick={(e)=>{setValue(e.target.value)}}/>
     );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* 
-【React.js入門 - 21】 各階層Component的溝通 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
-https://ithelp.ithome.com.tw/articles/10223754
+【React.js入門 - 22】 元件練習(上) - 在class利用遞迴+state實作動畫 - iT 邦幫忙::一起幫忙解決難題，拯救 IT 人的一天
+https://ithelp.ithome.com.tw/articles/10224160
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
